aaea107d1957bccf3450df891b99cc27fba24375
JQ\jieqiang <jieqiangt@gmail.com>
2025-02-13 12:42:48 +0800

    added scripts & utils to chunk, process & predict audio file

--- data_utils.py
+++ data_utils.py
@@ -215,8 +215,6 @@ def split_audio_dataset():
 
 
 def create_dataloader(labels_path, data_path, batch_size=12, shuffle=True, seed=42):
-    """Make PyTorch DataLoaders for train / developement / evaluation"""
-    # track = config["track"]
 
     labels, file_list = generate_datalist(dir_meta=labels_path, with_pred=True)
     print("no. files in  dataloader:", len(file_list))
@@ -235,3 +233,40 @@ def create_dataloader(labels_path, data_path, batch_size=12, shuffle=True, seed=
                              generator=gen)
 
     return data_loader
+
+
+def pad_audio(x, max_len=32300):
+    x_len = x.shape[0]
+    # need to pad
+    num_repeats = int(max_len / x_len) + 1
+    padded_x = np.tile(x, (1, num_repeats))[:, :max_len][0]
+    return padded_x
+
+
+def split_audio(audio, start_range, sample_size, max_audio_size):
+
+    end_range = start_range + sample_size
+
+    if end_range > max_audio_size:
+        ending_audio = audio[start_range:max_audio_size]
+        split_audio = pad_audio(ending_audio)
+        return split_audio
+
+    split_audio = audio[start_range:end_range]
+    return split_audio
+
+
+def preprocess_audio_for_cnn(X_raw):
+
+    FRAME_SIZE = 1024
+    HOP_SIZE = int(FRAME_SIZE/4)
+
+    X_stft = librosa.stft(y=X_raw, n_fft=FRAME_SIZE, hop_length=HOP_SIZE)
+    X_log_stft = librosa.power_to_db(np.abs(X_stft)**2)
+    X_delta = librosa.feature.delta(X_log_stft, width=9, order=1)
+    X_delta2 = librosa.feature.delta(X_log_stft, width=9, order=2)
+    stacked = [arr.reshape((1, X_log_stft.shape[0], X_log_stft.shape[1]))
+               for arr in (X_log_stft, X_delta, X_delta2)]
+    X = torch.FloatTensor(np.concatenate(stacked, axis=0))
+
+    return X
--- scripts.py
+++ scripts.py
@@ -2,6 +2,8 @@ import time
 import torch
 from data_utils import tally_correct_preds
 import numpy as np
+from data_utils import split_audio, preprocess_audio_for_cnn
+import math
 
 
 def predict(model, test_dataloader, criterion, device):
@@ -82,3 +84,35 @@ def train(model, train_dataloader, val_dataloader, criterion, optimizer, epochs,
     total_elapsed_time = time.time() - start_time
 
     return train_losses, train_correct, val_losses, val_correct, total_elapsed_time, y_pos_probs, y_tests
+
+
+def predict_single_audio(audio, model):
+
+    SAMPLE_RATE = 16000
+    # split audio into ~2s chunks
+    sample_size = 32300
+    max_audio_size = audio.shape[0]
+    audio_splits = []
+    for start_range in range(0, max_audio_size, sample_size):
+        audio_split = split_audio(
+            audio, start_range, sample_size, max_audio_size)
+        audio_splits.append(audio_split)
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    if device == "cpu":
+        raise ValueError("GPU not detected!")
+
+    model = model.to(device)
+
+    y_probs = []
+    for X_raw in audio_splits:
+
+        X = preprocess_audio_for_cnn(X_raw)
+        X = X.to(device)
+        y = model(X)
+        y_probs.extend(np.repeat(y.detach().cpu().numpy()[0][1], 2).tolist())
+
+    num_secs = math.ceil(audio.shape[0]/SAMPLE_RATE)
+    y_probs = np.array(y_probs[0:num_secs])
+
+    return y_probs
