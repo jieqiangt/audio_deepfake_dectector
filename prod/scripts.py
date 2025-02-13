586e01a87a50747b8cf56db512d7cc4d3ecc95f7
JQ\jieqiang <jieqiangt@gmail.com>
2025-02-13 14:14:56 +0800

    - output changed to python float dtype rather than numpy dtype

--- prod/scripts.py
+++ prod/scripts.py
@@ -28,19 +28,19 @@ def predict_single_audio(audio, model):
         X = preprocess_audio_for_cnn(X_raw)
         X = X.to(device)
         y = model(X)
-        y_probs.extend(np.repeat(y.detach().cpu().numpy()[0][1], 2).tolist())
+        y_probs.extend(np.repeat(y.detach().cpu().tolist()[0][1], 2).tolist())
 
     num_secs = math.ceil(audio.shape[0]/SAMPLE_RATE)
-    y_probs = np.array(y_probs[0:num_secs])
+    y_probs = y_probs[0:num_secs]
 
     # threshold to determine positive label
-    threshold = 0.7
+    threshold = 0.6
 
     # get labels based on threshold
     labels = [1 if score >= threshold else 0 for score in y_probs]
 
     # get agg label for whole audio
-    # logic: check if 3 consecutive sections is labelled as 1, if it is the whole audio is declared as fake
+    # logic: check if 3 consecutive seconds is labelled as 1, if it is the whole audio is declared as fake
     agg_label = 0
     for i in range(len(labels)-2):
         if labels[i] == 1 and labels[i+1] == 1 and labels[i+2] == 1:
@@ -51,12 +51,11 @@ def predict_single_audio(audio, model):
     if agg_label == 1:
         # if it is declared fake, get all probabilities above threshold and average them
         probs = list(filter(lambda score: score >= threshold, y_probs))
-        print(probs)
-        confidence = np.average(probs)
+        confidence = np.average(probs).item()
     else:
         # if it is declared fake, get all probabilities above threshold and average them
         probs = list(filter(lambda score: score <= threshold, y_probs))
-        confidence = np.average(probs)
+        confidence = np.average(probs).item()
 
     y_probs_ts = list(zip(range(0, num_secs), y_probs))
 
